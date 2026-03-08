0c4db09fb0e6295ae3e5d455b6e795b6fd0ca6bd
Itodo-S <itodosimonitodo1@gmail.com>
2026-03-08 20:50:33 +0100

    minor changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, 
         globPatterns: ['**/*.{js,css,html,svg,woff2}'],
         runtimeCaching: [
           {
